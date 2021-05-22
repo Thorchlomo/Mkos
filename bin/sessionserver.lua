544b3b92e67c168609754cf2aa286c47044f16be
Thorchlomo <henry.naitlho@gmail.com>
2021-05-22 14:53:12 +0200

    Added readme.md, and sessionserver

---
+++ bin/sessionserver.lua
@@ -0,0 +1,24 @@
+local component = require("component")
+local event = require("event")
+local m = component.modem
+
+local port = 1441
+
+
+print("Session system by thorchlomo")
+print("Initializating Session Server")
+
+m.open(port) --Use of 1441 port
+io.stderr:write("Openned port 1441 :" m.isOpen(port))
+
+
+while true do
+	local _, _, from, portused, _, message = event.pull("modem_message")
+	messageR = tostring(message)
+	print("Got a message from " .. from .. " on port " .. portused .. ": " .. messageR)
+	if string.match(messageR, ".") then 
+		print("This message appear be an session asking, consider it is")
+		sessionName = string.sub(0,string.sub(string.find(messageR, "."), 0, 0)
+		print("Session Name : " .. sessionName)
+	end
+end
---
+++ readme.md
@@ -0,0 +1,5 @@
+Mkos, an OpenOS version that integrate session
+
+Session Fonctonement :
+1 session server, who host file
+infinite (or near) session client (just computer with session.lua command)
