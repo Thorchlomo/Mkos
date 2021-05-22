6479898a3a0eacc50e290d81eef4e0d2baf3e2b4
Thorchlomo <henry.naitlho@gmail.com>
2021-05-22 13:16:36 +0200

    Added Installer

---
+++ installer.lua
@@ -0,0 +1,19 @@
+
+function file_exist(file) 
+	local file = io.open(file, "r")
+	if file~=nil then 
+		io.close()
+		return true
+	else
+		return false
+	end
+
+
+print("Removing old version if they exist")
+if file_exist("/bin/session.lua") then 
+	os.remove("/bin/session.lua")
+	print("removed session.lua")
+	end
+
+print("Removed all old file, downloading new")
+os.execute("wget https://raw.githubusercontent.com/Thorchlomo/Mkos/main/bin/session.lua?token=ARPS2KTEI7UKBZOOBSFQQKDAVDUDM")
