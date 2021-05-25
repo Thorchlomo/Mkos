08c41136852f79f630088de3c743ed112d76942f
Thorchlomo <henry.naitlho@gmail.com>
2021-05-25 12:09:30 +0200

    Added help to session.lua

--- bin/session.lua
+++ bin/session.lua
@@ -1,1 +1,19 @@
 io.stderr:write("Session system by thorchlomo")
+
+local session_host = nil --This is the adress of the session server, must be seted before use
+
+local shell = require("shell")
+local args, options = shell.parse(...)
+
+if #args < 3 then	
+	io.write("Usage: session <user> <password> [g/p]\n")
+	io.write(" g: Download existing session from session server.\n")
+	io.write(" p: Put actual session to session server.\n")
+	io.write(" P.S: using the shutdown command, session is automaticaly pushed to server.")
+	return
+ end
+
+ if session_host == nil then
+	io.write("An session server must be seted on /bin/session.lua")
+	return
+end
