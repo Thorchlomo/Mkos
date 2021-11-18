6e737c8743a65499cba2cc670d2be323d6de8e2e
Thorchlomo <henry.naitlho@gmail.com>
2021-11-18 15:10:50 +0100

    mkapt now make an request to an dummy packet for testing purpose, added help

--- bin/mkapt.lua
+++ bin/mkapt.lua
@@ -1,7 +1,36 @@
-if #args < 3 then	
-	io.write("Usage: session <user> <password> [g/p]\n")
-	io.write(" g: Download existing session from session server.\n")
-	io.write(" p: Put actual session to session server.\n")
-	io.write(" P.S: using the shutdown command, session is automaticaly pushed to server.")
+local component = require("component")
+local internet = require("internet")
+local inspect = require("inspect")
+local shell = require("shell")
+
+local args, options = shell.parse(...) -- Récuperation des arguments et des options
+
+if #args < 2 then	
+	io.write("Usage: mkatp <get/update/add-src/remove-src> <packet/src-url>\n")
+	io.write(" get : Used to download packet from all source server available.\n")
+	io.write(" update : Update all the packet (if needed only).\n")
+	io.write(" add-src : Add an source server.\n")
+	io.write(" remove-src : Make the opposite.\n")
+	io.write(" P.S. : This program require an internet card to work")
 	return
  end
+
+ if not component.isAvailable("internet") then
+  io.stderr:write("This program requires an internet card to run.")
+  return
+end
+
+if args[1] == "get" then -- The user want to recover an packet from packet server
+	local packet = args[2]
+	print("Looking for packet : "packet)
+ 
+	local handle = internet.request("https://raw.githubusercontent.com/Thorchlomo/Mkos/main/etc/mkapt/somepacket")
+	local result = ""
+	local mt = getmetatable(handle)
+ 
+	-- The response method grabs the information for
+	-- the HTTP response code, the response message, and the
+	-- response headers.
+	local code, message, headers = mt.__index.response()
+	print("code = "..tostring(code))
+end	
