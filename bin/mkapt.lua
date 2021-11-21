59014eaf347986652691e43b12db30ac39ecfd1f
Thorchlomo <henry.naitlho@gmail.com>
2021-11-21 13:37:31 +0100

    Updated packte.list

---
+++ bin/mkapt.lua
@@ -0,0 +1,59 @@
+local filesystem = require("filesystem")
+local component = require("component")
+local internet = require("internet")
+local shell = require("shell")
+
+local args, options = shell.parse(...) -- Récuperation des arguments et des options
+
+if #args < 2 then	
+	io.write("Usage: mkatp <get/update/upgrade/add-src/remove-src> <packet/src-url>\n")
+	io.write(" get : Used to download packet from all source server available.\n")
+	io.write(" update : Update the packet list.\n")
+	io.write(" upgrade : upgrade all the packet or the specified packet")
+	io.write(" add-src : Add an source server.\n")
+	io.write(" remove-src : Make the opposite.\n")
+	io.write(" P.S. : This program require an internet card to work")
+	return
+ end
+
+ if not component.isAvailable("internet") then
+  io.stderr:write("This program requires an internet card to run.")
+  return
+end
+
+-- Start of the "main" code
+function read_source()
+	local config = {} -- Contain the list of the file used for analyze
+	local p = io.popen('find "/etc/mkapt/" --type=f')  --Open directory look for files     
+	for file in p:lines() do                         -- Recovering all the file name in the folder 
+		print(file)       
+		table.insert(config, file)
+	end
+
+	for cle, valeur in ipairs(config) do
+		lines = {}
+		for line in io.lines(valeur) do 
+			lines[#lines + 1] = line
+		end
+	end
+
+	return config
+
+end
+
+if args[1] == "get" then -- The user want to recover an packet from packet server
+	os.execute("mkdir /etc/mkapt/"..args[2])
+	os.execute("wget https://github.com/Thorchlomo/Mkos/blob/main/etc/mkapt/"..args[2].."/installer.lua -f /etc/mkapt/"..args[2].."/installer.lua")
+	local installer = require("/etc/mkapt/"..args[2].."/installer.lua")
+	print(installer.ver())
+end	
+
+if args[1] == "update" then
+	print("Updating package list for the following source :",args[2])
+	os.setenv("/etc/mkapt")
+	os.execute("wget "..args[2].." -f")
+end
+
+if args[1] == "debug" then
+	read_source()
+end
