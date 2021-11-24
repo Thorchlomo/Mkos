aa593cf5962d2fbd818e4ca25e843159ecec6397
Thorchlomo <henry.naitlho@gmail.com>
2021-11-24 18:42:19 +0100

    Updated Dummy Package Make the installer remove all the mkapt dependancies. Updated default source, they now point to rawgithub and no longer github

--- etc/mkapt/dummy/installer.lua
+++ etc/mkapt/dummy/installer.lua
@@ -1,5 +1,21 @@
 local version = "1.0"
-print("installing...")
+print("Initializting Dummy Packet !")
+
 function ver()
-  return version
+	return version
+end
+
+function install()
+	return "I'm a lonely packet"
+end
+
+function remove()
+	print("I'm sad, why did you want to do that")
+	os.execute("rm /etc/mkapt/dummy/installer.lua")
+	return "I'm Broken !"
+end
+
+function update()
+	install()
+	return "ready to start !"
 end
--- installer.lua
+++ installer.lua
@@ -26,7 +26,8 @@ if file_exist("/bin/session.lua") then
 -- Mkapt, the command used to interract with packet manager
 if file_exist("/bin/mkapt.lua") then 
 	os.execute("rm /bin/mkapt.lua")
-	print("removed mkapt.lua")
+	os.execute("rmdir /etc/mkapt")
+	print("removed mkapt.lua and dependancies")
 	end
 
 -- Session Server, the command who start define an computer as an session hoster
@@ -48,12 +49,14 @@ if indev == false then
 	os.execute("wget https://raw.githubusercontent.com/Thorchlomo/Mkos/main/bin/sessionserver.lua /bin/sessionserver.lua")
 	os.execute("wget https://raw.githubusercontent.com/Thorchlomo/Mkos/main/usr/misc/greetings.txt /usr/misc/greetings.txt")
 	os.execute("wget https://raw.githubusercontent.com/Thorchlomo/Mkos/main/bin/mkapt.lua /bin/mkapt.lua")
+	os.execute("wget https://raw.githubusercontent.com/Thorchlomo/Mkos/main/etc/mkapt/packet.list /etc/mkapt/packet.list -f")
 	
 else 
 	os.execute("wget https://raw.githubusercontent.com/Thorchlomo/Mkos/thorchlomo-indev/bin/session.lua /bin/session.lua")
 	os.execute("wget https://raw.githubusercontent.com/Thorchlomo/Mkos/thorchlomo-indev/bin/sessionserver.lua /bin/sessionserver.lua")
 	os.execute("wget https://raw.githubusercontent.com/Thorchlomo/Mkos/thorchlomo-indev/usr/misc/greetings.txt /usr/misc/greetings.txt")
 	os.execute("wget https://raw.githubusercontent.com/Thorchlomo/Mkos/thorchlomo-indev/bin/mkapt.lua /bin/mkapt.lua")
+	os.execute("wget https://raw.githubusercontent.com/Thorchlomo/Mkos/thorchlomo-indev/etc/mkapt/packet.list /etc/mkapt/packet.list -f")
 	end
 
 print("creating mkapt folder in /etc")
